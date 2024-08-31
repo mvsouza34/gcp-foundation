8fe695599eda9b1c5ac545cafa89edbebea65ca1
temcarlosrocha <carlos.rocha@ipnet.cloud>
2024-08-31 19:40:55 -0300

    udpate with export vpc

--- 04-compute-engine/mod-gke-vpc.tf
+++ 04-compute-engine/mod-gke-vpc.tf
@@ -27,41 +27,6 @@ module "vpc" {
             },
         ]
     }
-    
-
-
-
-
-    #     {
-    #         subnet_name           = "subnet-02"
-    #         subnet_ip             = "10.10.20.0/24"
-    #         subnet_region         = "us-west1"
-    #         subnet_private_access = "true"
-    #         subnet_flow_logs      = "true"
-    #         description           = "This subnet has a description"
-    #     },
-    #     {
-    #         subnet_name               = "subnet-03"
-    #         subnet_ip                 = "10.10.30.0/24"
-    #         subnet_region             = "us-west1"
-    #         subnet_flow_logs          = "true"
-    #         subnet_flow_logs_interval = "INTERVAL_10_MIN"
-    #         subnet_flow_logs_sampling = 0.7
-    #         subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
-    #     }
-    # ]
-
-    # secondary_ranges = {
-    #     subnet-01 = [
-    #         {
-    #             range_name    = "subnet-01-secondary-01"
-    #             ip_cidr_range = "192.168.64.0/24"
-    #         },
-    #     ]
-
-    #     subnet-02 = []
-    # }
-
     routes = [
         {
             name                   = "egress-internet"
@@ -70,14 +35,6 @@ module "vpc" {
             tags                   = "egress-inet"
             next_hop_internet      = "true"
         },
-        # {
-        #     name                   = "app-proxy"
-        #     description            = "route through proxy to reach app"
-        #     destination_range      = "10.50.10.0/24"
-        #     tags                   = "app-proxy"
-        #     next_hop_instance      = "app-proxy-instance"
-        #     next_hop_instance_zone = "us-west1-a"
-        # },
     ]
 }
 
@@ -95,4 +52,9 @@ output "subnet_cidr_cluster_range_secondary_services" {
 }
 output "subnet_cidr_cluster_range_secondary_pods" {
     value = "module.vpc.secondary_ranges.subnet-02[0].range_name"
+}
+
+resource "local_file" "aiqfome_cluster_vpc" {
+    content  = module.vpc.network_name
+    filename = "${path.module}/aiqfome_cluster_vpc.txt"
 }
